e66768ed798424de3b73d2201d123def3392225e
Christopher Watson <christopherwatson@christophersmbp.mynetworksettings.com>
2023-04-06 21:48:45 -0400

    cors for lametric

--- app.js
+++ app.js
@@ -11,9 +11,18 @@ require('dotenv').config();
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'))
-   .use(cookieParser())
-   .use(cors());
+app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.options('*', cors());
+// app.use((req, res, next) => {
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Headers', '*');
+//    if (req.method === 'OPTIONS') {
+//       res.headers('Access Control-Allow-Methods', 'POST, GET');
+//       return res.status(200).json({});
+//    }
+//    next();
+// });
 
 const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
